96211fb2e610218d3c29c5af7188e1a03a197d29
Buchi Rex-David <rhexmilia06@gmail.com>
2026-03-13 19:49:09 -0700

    feat: implement role-based permissions and restrict workout assignments to trainer's clients

--- gym/admin.py
+++ gym/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import User, TrainerProfile, ClientProfile, Workout, WorkoutAssignment
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['role']
+
+
+
+class TrainerProfileAdmin(admin.ModelAdmin):
+    list_display = ['user']
+
+
+class ClientProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'trainer']
+
+
+class WorkoutAdmin(admin.ModelAdmin):
+    list_display = ['title', 'duration', 'difficulty']
+
+
+class WorkoutAssignmentAdmin(admin.ModelAdmin):
+    list_display = ['client', 'trainer', 'workout']
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(TrainerProfile, TrainerProfileAdmin)
+admin.site.register(ClientProfile, ClientProfileAdmin)
+admin.site.register(Workout, WorkoutAdmin)
+admin.site.register(WorkoutAssignment, WorkoutAssignmentAdmin)
